77215263ce88ae76f649eae5fd6e59eda1b4e2f6
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-08-14 17:40:16 +0200

    new Location page

--- src/components/page/NewLocationPage.tsx
+++ src/components/page/NewLocationPage.tsx
@@ -1,10 +1,44 @@
+import { Button } from "@/static/Button";
+import { Form } from "@/static/forms/Form";
+import { Input } from "@/static/forms/Input";
+import { IconLeft } from "@/static/icons/IconLeft";
 import type { Component } from "solid-js";
-import { PageType } from "./PageTypes";
+import type { PageType } from "./PageTypes";
 
-type PageProps = {
-  link: (page: PageType) => void;
-};
+type PageProps = { link: (page: PageType) => void };
 
-export const NewLocationPage: Component<PageProps> = () => {
-  return <h1>New Locatio</h1>;
+export const NewLocationPage: Component<PageProps> = ({ link }) => {
+  return (
+    <div class="hhh-spacer">
+      <h3 class="title is-3 has-text-centered">Neues Restaurant</h3>
+      <Form>
+        <Input
+          label="Restaurant"
+          placeholder="Name des Restaurant"
+          helptext="Der Name des Restaurants, der überall verwendet wird."
+        />
+        <Input
+          label="Link"
+          placeholder="https://..."
+          helptext="Unter welchem Link das Menü des Restaurants eingesehen werden kann."
+        />
+        <div
+          class="pt-5 is-flex is-flex-wrap-wrap is-justify-content-space-evenly"
+          style="gap: 1rem;"
+        >
+          <Button
+            color="danger"
+            outlined={true}
+            size="large"
+            onClick={() => link("newOrder")}
+          >
+            <IconLeft icon="trash">Abbrechen</IconLeft>
+          </Button>
+          <Button color="success" size="large" onClick={() => link("newOrder")}>
+            <IconLeft icon="check">Restaurant hinzufügen</IconLeft>
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };

84d903d10c779377862d8607aeda429b398a0f7f
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-08-14 13:53:57 +0200

    add source map for styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './bulmaswatch-lumen.min.css';
+import './style/bulmaswatch-lumen.min.css';
 import App from './App';
 
 render(() => <App />, document.getElementById('root') as HTMLElement);

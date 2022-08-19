2e5d3782fd0c24084db26c589ed75e2fcab98659
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-08-19 14:52:47 +0000

    Shorten types

--- src/api/ApiTypes.ts
+++ src/api/ApiTypes.ts
@@ -1,68 +1,24 @@
+import { EntryState, OrderState, RestaurantState } from "@util/StateTypes";
 import { DateTime } from "luxon";
 
 export type RestaurantStatus = "active" | "inactive" | "deleted";
 export type OrderStatus = "auto" | "active" | "inactive" | "deleted";
 export type EntryStatus = "active" | "inactive" | "deleted";
 
-export type RestaurantPost = {
-  kind: "restaurant";
-  id: string;
-  label: string;
-  menuLink: string;
-  comment: string;
+export type RestaurantPost = Omit<RestaurantGet, "timestamp">;
+export type OrderPost = Omit<OrderGet, "timestamp">;
+export type EntryPost = Omit<EntryGet, "timestamp">;
+
+export type RestaurantGet = Omit<RestaurantState, "status"> & {
   status: RestaurantStatus;
 };
 
-export type OrderPost = {
-  kind: "order";
-  id: string;
-  restaurantId: string;
-  orderer: string;
-  comment: string;
-  timeWindow: number;
+export type OrderGet = Omit<OrderState, "status"> & {
   status: OrderStatus;
 };
 
-export type EntryPost = {
-  kind: "entry";
-  id: string;
-  orderId: string;
-  eater: string;
-  menuItem: string;
-  comment: string;
+export type EntryGet = Omit<EntryState, "status"> & {
   status: EntryStatus;
-};
-
-export type RestaurantGet = {
-  kind: "restaurant";
-  id: string;
-  label: string;
-  menuLink: string;
-  comment: string;
-  status: RestaurantStatus;
-  timestamp: DateTime;
-};
-
-export type OrderGet = {
-  kind: "order";
-  id: string;
-  restaurantId: string;
-  orderer: string;
-  comment: string;
-  timeWindow: number;
-  status: OrderStatus;
-  timestamp: DateTime;
-};
-
-export type EntryGet = {
-  kind: "entry";
-  id: string;
-  orderId: string;
-  eater: string;
-  menuItem: string;
-  comment: string;
-  status: EntryStatus;
-  timestamp: DateTime;
 };
 
 export const isRestaurant = (o: any): o is Omit<RestaurantGet, "timestamp"> => {

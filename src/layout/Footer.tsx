fefcdb90a7ec52d6e9888faa8c760a243008c244
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-09-19 18:16:35 +0100

    Update Date in Footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: Component = () => {
   return (
     <footer class="footer has-background-dark">
       <div class="content has-text-centered">
-        <p class="has-text-white-ter">Zuletzt aktualisiert: August 2022</p>
+        <p class="has-text-white-ter">Zuletzt aktualisiert: September 2022</p>
       </div>
     </footer>
   );
